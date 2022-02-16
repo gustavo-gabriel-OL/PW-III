46e9e10b3867afbc2bdd4fecd7cc762415a0241a
gustavogbriel2004@gmail.com <gustavogbriel2004@gmail.com>
2022-02-16 08:27:50 -0300

    .-.

---
+++ aula_02/modelo/function.php
@@ -0,0 +1,31 @@
+<?php
+
+    $NOME = $_REQUEST ['NOME'];
+    $ANIMAL =$_REQUEST ['ANIMAL'];
+
+    if(empty($NOME)){
+        $dados = array(
+            "tipo" => 'error',
+            "mensagem" => 'Existe(m) campo(s) a ser(em) preenchido(s).'
+        );
+    }else{
+        switch($ANIMAL){
+            case '1': $dados = array(
+                "tipo" => 'cachorro',
+                "mensagem" => 'Seja bem  vindo, '.$NOME.' , sabemos que o seu animal de preferência é o cachorro'
+            );
+            break;
+            case '2': $dados = array(
+                "tipo" => 'gato',
+                "mensagem" => 'Seja bem  vindo, '.$NOME.' , sabemos que o seu animal de preferência é o gato'
+            );
+            break;
+            case '3': $dados = array(
+                "tipo" => 'peixe',
+                "mensagem" => 'Seja bem  vindo, '.$NOME.' , sabemos que o seu animal de preferência é o peixe'
+            );
+            break;
+        }
+    }
+
+    echo json_decode($dados);
