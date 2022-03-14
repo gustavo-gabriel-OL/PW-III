45740e5b21b3f4896bbf3ccdb8f3215d06d12348
gustavo-gabriel-OL <gustavogbriel2004@gmail.com>
2022-03-14 17:24:57 -0300

    ;-;

--- aula_03/controle/function.js
+++ aula_03/controle/function.js
@@ -1,33 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.btn-send').click(function(e) {
+    $('.btn-send').click(function (e) {
         e.preventDefault();
 
-        let dados = $('#form').serialize()
 
-        $('#retorno').empty()
+        let dados = $("#form").serialize();
+
+        console.log(dados);
+
+        $('#retorno').empty();
 
         $.ajax({
-            type: 'POST',
-            dataType: 'JSON',
+            type: "POST",
+            dataType: "JSON",
             assync: true,
             data: dados,
-            url: '../modelo/retorno.php',
-            success: function(dados){
+            url: "../modelo/retorno.php",
+            success: function (dados) {
                 $('#retorno').append(`
-                    <div class="col-12 col-sm-8 col-md-6">
-                        <div class="alert-primary">
-                            <h1 class="text-white text-center">
-                                ${dados.mensagem}
-                            </h1>
+                    <div id="retorno" class="row justify-content-center">
+                        <div class="col-13 col-sm-8 col-md-6">
+                            <div class="${dados.B4}">
+                                <h1 class="text-center text-dark">
+                                    ${dados.mensagem}
+                                </h1>
+                            </div>
                         </div>
-                        <div class="mb-5">
-                            <img src="../../img2/${dados.tipo}" class="img-fluid">
-                        </div>
-                        
-                    </div>
-                `)
+                    </div>               
+               `);
             }
-        })
-    })
-})
+        });
+    });
+});
